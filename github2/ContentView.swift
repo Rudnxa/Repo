081b62179c8acaa45b13f90d1c2405de7214b7b2
Xavier <rundxa@student.breckschool.org>
2021-05-03 14:02:25 -0500

    Initial Commit

---
+++ github2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github2
+//
+//  Created by Xavier Rudnick on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github2/github2App.swift
@@ -0,0 +1,17 @@
+//
+//  github2App.swift
+//  github2
+//
+//  Created by Xavier Rudnick on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct github2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
